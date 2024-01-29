fea3918dd938aaa958036f433e9d805e649b6306
THARMATT <nigamsharma23112001@gmail.com>
2024-01-29 19:36:32 +0530

    tailwindcss

---
+++ week-8/TailwindCSS/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RevenueCard from './Components/RevenueCard'
+
+function App() {
+  return (
+    <>
+    <RevenueCard  />
+    </>
+   
+  )
+}
+
+export default App
